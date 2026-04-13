de9628483ff08c655aeda9c2f2a68bfe2023b661
anjawa <arsalan.jawaid@proton.me>
2026-04-13 17:09:53 +0200

    newline added

--- src/nsgp/lowrank/__init__.py
+++ src/nsgp/lowrank/__init__.py
@@ -1,4 +1,4 @@
 from .nff import NonstationaryFeatures
 from .random_nff import RandomNonstationaryFeatures
 
-__all__ = ["NonstationaryFeatures", "RandomNonstationaryFeatures"] 
+__all__ = ["NonstationaryFeatures", "RandomNonstationaryFeatures"]
