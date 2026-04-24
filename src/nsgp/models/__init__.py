923777e4ba7f04f5a8d6473206e98c47a71c7263
anjawa <arsalan.jawaid@proton.me>
2026-04-24 22:30:20 +0200

    placeholder

--- src/nsgp/models/__init__.py
+++ src/nsgp/models/__init__.py
@@ -1,9 +1,11 @@
 from .sdn_factorized import FactorizedSpectralDensityNetwork
 from .neural_gsm_gp import NeuralGSMGP
 from .standard_gp import StandardGP
+from .dkl_gp import DKLGP
 
 __all__ = [
     "FactorizedSpectralDensityNetwork",
     "NeuralGSMGP",
     "StandardGP",
+    "DKLGP"
 ]
